cf4dd809cf3dd225e3a95cbabdab023c576e2a68
Nuo Ma <2500646875@qq.com>
2021-03-11 15:48:30 +0800

    Update index.py
    count term frequency for every document

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import re
 import nltk
 import sys
 import getopt
+import os
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
@@ -13,8 +14,46 @@ def build_index(in_dir, out_dict, out_postings):
     then output the dictionary file and postings file
     """
     print('indexing...')
-    # This is an empty method
-    # Pls implement your code in below
+    indexer = indexing(in_dir, out_dict, out_postings)
+    indexer.perform_indexing()
+
+class indexing:
+
+    def __init__(self, in_dir, dictionary, posting):
+        self.dict = {}
+        self.dir = in_dir
+        self.dict_file = dictionary
+        self.posting_file = posting
+        self.dictionary = {}
+
+
+    def perform_indexing(self):
+
+        for filename in self.all_docID:
+            count = self.count_doc(os.path.join(self.dir, filename))
+            self.merge_dic(filename, count)
+            break
+
+    def count_doc(self, file_path):
+        punctuation = ['.', ',', ':', "'", '!', '?', "&", ";", ">", "<", "`", "'", "/", "+", "[", "]"]
+        f = open(file_path, 'r')
+        tokens = [nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(f.read())]
+        tokens = [nltk.PorterStemmer().stem(re.sub(r'[./\-!?^+&%$#()=*:`,"\']', '', word.lower())) for sent in
+                  tokens for word in sent if word not in punctuation and not word.isdigit()]
+
+        count = {}
+        for token in tokens:
+            if token not in count.keys():
+                count[token] = 1
+            else:
+                count[token] += 1
+
+        return count
+
+    def merge_dic(self, docID, dict):
+
+        return
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
